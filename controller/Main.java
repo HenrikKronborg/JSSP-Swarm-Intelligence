6d4cc93c01e655a326a41fc7722241a0470569b8
Henrik Kronborg <henrkron@stud.ntnu.no>
2019-03-15 17:59:39 +0100

    Added use of JavaFX

--- controller/Main.java
+++ controller/Main.java
@@ -1,18 +1,25 @@
 package controller;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import model.Job;
 import model.Step;
 import model.utils.Data;
 
 import java.util.ArrayList;
 
-public class Main {
+public class Main extends Application {
     public static int n = 0; // Number of jobs
     public static int m = 0; // Number of machines
 
     public static ArrayList<Job> jobs = new ArrayList<>();
 
     public static void main(String[] args) {
+        launch(args);
+
         Data data = new Data();
         data.ReadData("./src/test_data/1.txt");
 
@@ -27,5 +34,13 @@ public class Main {
             System.out.println("##############################################");
         }
         */
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainScreen.fxml"));
+        primaryStage.setTitle("JSSP");
+        primaryStage.setScene(new Scene(root));
+
+        primaryStage.show();
     }
 }

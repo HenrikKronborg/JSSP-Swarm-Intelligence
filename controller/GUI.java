100479c56c6410c2750a343fe62fd2920fc154a6
stianStensli <stian.hs@hotmail.com>
2019-03-21 12:47:45 +0100

    Code clean up, removed example

--- controller/GUI.java
+++ controller/GUI.java
@@ -36,44 +36,7 @@ public class GUI implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initColor();
-
-        String[] machines = new String[] { "Machine 1", "Machine 2", "Machine 3" };
-
-        xAxis.setLabel("");
-        xAxis.setTickLabelFill(Color.CHOCOLATE);
-        xAxis.setMinorTickCount(4);
-
-        yAxis.setLabel("");
-        yAxis.setTickLabelFill(Color.CHOCOLATE);
-        yAxis.setTickLabelGap(10);
-        yAxis.setCategories(FXCollections.<String>observableArrayList(Arrays.asList(machines)));
-
-        chart.setTitle("Job schedule");
-        chart.setLegendVisible(false);
-        chart.setBlockHeight( 50);
-        String machine;
-
-        machine = machines[0];
-        XYChart.Series series1 = new XYChart.Series();
-        series1.getData().add(new XYChart.Data(0, machine, new GanttChart.ExtraData( 1, "status-red")));
-        series1.getData().add(new XYChart.Data(2, machine, new GanttChart.ExtraData( 1, "status-red")));
-        series1.getData().add(new XYChart.Data(3, machine, new GanttChart.ExtraData( 1, "status-green")));
-
-        machine = machines[1];
-        XYChart.Series series2 = new XYChart.Series();
-        series2.getData().add(new XYChart.Data(0, machine, new GanttChart.ExtraData( 1, "status-green")));
-        series2.getData().add(new XYChart.Data(1, machine, new GanttChart.ExtraData( 1, "status-green")));
-        series2.getData().add(new XYChart.Data(2, machine, new GanttChart.ExtraData( 2, "status-red")));
-
-        machine = machines[2];
-        XYChart.Series series3 = new XYChart.Series();
-        series3.getData().add(new XYChart.Data(0, machine, new GanttChart.ExtraData( 1, "status-blue")));
-        series3.getData().add(new XYChart.Data(1, machine, new GanttChart.ExtraData( 2, "status-red")));
-        series3.getData().add(new XYChart.Data(3, machine, new GanttChart.ExtraData( 1, "status-green")));
-
-        chart.getData().addAll(series1, series2, series3);
-
-        chart.getStylesheets().add(getClass().getResource("../view/ganttchart.css").toExternalForm());
+        initChart();
 
     }
 
@@ -89,7 +52,25 @@ public class GUI implements Initializable {
         }
 
     }
+    private void initChart(){
+        String[] machines = new String[] { "Machine 1", "Machine 2", "Machine 3" };
 
+        xAxis.setLabel("");
+        xAxis.setTickLabelFill(Color.CHOCOLATE);
+        xAxis.setMinorTickCount(4);
+
+        yAxis.setLabel("");
+        yAxis.setTickLabelFill(Color.CHOCOLATE);
+        yAxis.setTickLabelGap(10);
+        yAxis.setCategories(FXCollections.<String>observableArrayList(Arrays.asList(machines)));
+
+        chart.setTitle("Job schedule");
+        chart.setLegendVisible(false);
+        chart.setBlockHeight( 50);
+
+        chart.getStylesheets().add(getClass().getResource("../view/ganttchart.css").toExternalForm());
+
+    }
 
     @FXML
     public void run(){
@@ -128,7 +109,6 @@ public class GUI implements Initializable {
             }
             chart.getData().add(series);
         }
-        chart.getStylesheets().add(getClass().getResource("../view/ganttchart.css").toExternalForm());
     }
 
 }
